bb2c72f5451115f92c723a535602ddbf671a55b2
okrand <orkun.krand@gmail.com>
2017-08-24 12:41:00 -0400

    Add comments to crawler.py for easy reading

--- crawler.py
+++ crawler.py
@@ -26,81 +26,70 @@ for line in r.iter_lines(): #on each line if rel="memento" doesn't exist, ignore
 		if (line != lastline):
 			lastline = line
 			linkslist.append(line[1:line.find('>;')])
-			#print(line[1:line.find('>')])
-print(str(len(linkslist)) + " archive points found")
+print(str(len(linkslist)) + " archive points found") 
 
 lastdate = ''
-with open("test.txt", "r") as f:
-	for line in linkslist:
-		dateloc = line.find("/web/")
-		date = line[dateloc+5:dateloc+19] #[27:41]
-		#get one entry per month
-		if (date[:6] == lastdate): #if new month is the same as previous, skip
-			continue			
-		print(date)
-		res = urllib.urlopen(line)
-		html = res.read()
-		soup = BeautifulSoup(html, "lxml") #html.parser -> lxml
-		#get rid of scripts(javascript especially)
-		for elem in soup.findAll(['script', 'style']):
-			elem.extract()
-		if int(date) < 10120700000000:
-			#print(int(date)) < 0120700000000
-			continue
-		else:
+#with open("test.txt", "r") as f: 
+for line in linkslist:
+	dateloc = line.find("/web/")
+	date = line[dateloc+5:dateloc+19] #[27:41]
+	#get one entry per month
+	if (date[:6] == lastdate): #if new month is the same as previous, skip
+		continue			
+	print(date)
+	res = urllib.urlopen(line)
+	html = res.read()
+	soup = BeautifulSoup(html, "lxml") #html.parser -> lxml
+	#get rid of scripts(javascript especially)
+	for elem in soup.findAll(['script', 'style']):
+		elem.extract()
+	if int(date) < 10120700000000:
+		continue
+	else:
+		#try excepts that find the follower counts for different versions of Twitter since its beginning
+		try:
+			result = soup.select(".ProfileNav-item--followers")[0]
 			try:
-				result = soup.select(".ProfileNav-item--followers")[0]
-				try:
-					result = result.find("a")['title']
-				except:
-					result = result.find("a")['data-original-title']
+				result = result.find("a")['title']
+			except:
+				result = result.find("a")['data-original-title']
+		except:
+			try:
+				result = soup.select(".js-mini-profile-stat")[-1]['title']
 			except:
 				try:
-					result = soup.select(".js-mini-profile-stat")[-1]['title']
+					result = soup.select(".stats li")[-1].find("strong")['title']
 				except:
 					try:
-						result = soup.select(".stats li")[-1].find("strong")['title']
+						result = soup.select(".stats li")[-1].find("strong").text
 					except:
 						try:
-							result = soup.select(".stats li")[-1].find("strong").text
+							result = soup.select("#follower_count")[0].text
 						except:
 							try:
-								result = soup.select("#follower_count")[0].text
+								result = soup.select("#followers_count")[0].text
 							except:
 								try:
-									result = soup.select("#followers_count")[0].text
+									result = soup.select(".user-stats-followers")[0].text
+									#result = result[:result.find("Followers")]
 								except:
 									try:
-										result = soup.select(".user-stats-followers")[0].text
-										#result = result[:result.find("Followers")]
+										result = soup.select(".stats_count")[1].text
 									except:
-										try:
-											result = soup.select(".stats_count")[1].text
-										except:
-											print("Couldn't figure it out")
-											continue
-		result = re.sub(r'\D', '', result)
-		if (result == ''):
-			continue
-		#print(result)
-		# #result = result.split(' ',1)[0].encode("utf-8").strip()
-		# print(result)
-		# result = "{}".format(result)
-		# print(result)
-		# result = result.decode("utf-8").replace(" ", "")
-		# print(result)
-		# result = result.replace(",", "")
-		# result = result.replace(".", "")
-		try:
-			#result = "{:,d}".format(int(result))
-			print(result)
-			day = '-'.join([date[:4], date[4:6], date[6:8]])
-			w.write(day + ',' + result + '\n')
-			lastdate = date[:6]
-		except:
-			print("Number not latin")
-			continue						
+										print("Couldn't figure it out")
+										continue
+	result = re.sub(r'\D', '', result) #remove everything that's not a number
+	if (result == ''):
+		continue
+	try:
+		print(result)
+		day = '-'.join([date[:4], date[4:6], date[6:8]])
+		w.write(day + ',' + result + '\n')
+		lastdate = date[:6]
+	except:
+		print("Number not latin")
+		continue
 w.close()
-
+#Call the Rscript to create a linechart with the numbers collected
 Rcall = "Rscript --vanilla follower_count_linechart.r " + uname
 subprocess.call(Rcall, shell=True)
