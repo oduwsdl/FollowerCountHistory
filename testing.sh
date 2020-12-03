dbb09d5353b9d21c3a3127e1becbe223efe10991
mohammed siddique <naumansiddiqui4@gmail.com>
2020-12-03 11:31:11 -0500

    Remove extra testing script

--- testing.sh
+++ testing.sh
@@ -6,3 +6,4 @@ python3 -m pytest tests_datamanager.py
 python3 -m pytest tests_followeranalysis.py 
 python3 -m pytest tests_followerparser.py 
 python3 -m pytest tests_timemapdownloader.py
+docker push oduwsdl/fch
